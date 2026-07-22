22b0c4c221e6d09b5303166b8d2fa28b133cad3e
Maxim <maximlapunov@gmail.com>
2026-07-22 19:03:09 +0300

    Resolve client address migration ordering

--- backend/crm_app/migrations/0030_client_address_lat_client_address_lon.py
+++ backend/crm_app/migrations/0030_client_address_lat_client_address_lon.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("crm_app", "0028_client_address_details"),
+        ("crm_app", "0029_calculatorsettings"),
     ]
 
     operations = [
